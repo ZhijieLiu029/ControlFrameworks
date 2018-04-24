c3e476ffbf0979cef8dcd3f955e79d4cb2e2b2fb
ZhijieLiu029 <liuzhijie@nwafu.edu.cn>
2018-04-24 18:18:51 +0800

    modify to oo

---
+++ controller/plant.h
@@ -0,0 +1,26 @@
+#ifndef plant_h
+  #define plant_h
+#endif
+
+class PlantClass{
+  /* public variables and functions */
+  public:
+    // construction function
+    PlantClass();
+
+    
+    // init the plant object
+    bool Init();
+    
+    // refresh sensors
+    UpdateSensor();
+    AdjustPlant();
+
+    
+    
+  
+  private:
+    
+    
+};
+
