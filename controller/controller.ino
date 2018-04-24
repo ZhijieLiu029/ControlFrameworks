e3b0911479aa78d311ad8e0fbeb42d1f1a3e638d
ZhijieLiu029 <liuzhijie@nwafu.edu.cn>
2018-04-24 15:51:33 +0800

    finish the project

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,14 +1,68 @@
+/*
+ * This is an example for students.
+ * This project shows how to run control programs at a specific frequency.
+ * The globel variable TC is the program cycle. 
+ * Because the millis() will overflow after about 50 days,so the program should
+ * be ran less than 50 days.
+ * 
+ */
+
+#define TC 200             //控制周期(ms)
+//#define LED 13             //Led pin number
 
 
-int T  //控制周期
+/* timer variables */
+//unsigned long t_now;     //当前时刻
+unsigned long t_start;     //开始时刻
 
-
+/* globle variables */
+bool led_stat;
 void setup() {
 
+  // initialize communication if nessary
+  Serial.begin(115200);
+  
+  
+  // initialize actuator
+  /* 
+   *  After initialization, make sure every actuator is in an safe state.
+   *  
+   */
+ 
+  
+  // initialize sensors
+  
+  
+  // other initialization
+  led_stat = true;
+  
 }
-
 
 void loop() {
   
+  t_start = millis();
+  // update sensor
+
+
+  // control calculate
+  pinMode(LED_BUILTIN, OUTPUT);
+
+
+  // control output
+  Serial.println("Hello world!");
+  Serial.println(millis(),DEC);
+  if (led_stat){
+    digitalWrite(LED_BUILTIN,HIGH); 
+    
+  }else{
+    digitalWrite(LED_BUILTIN,LOW);
+    
+  }
+  led_stat = !led_stat;
+  
+
+  // just for time control
+  while(millis()-t_start<TC){
+    }
   
 }
